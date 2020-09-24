c83d210251d27a38376632711b1eb9489455303a
Laura Kreile <laura.kreile@inbox.lv>
2020-09-24 18:30:28 +0300

    repetition of lessons

---
+++ for_luminor.scala
@@ -0,0 +1,17 @@
+
+
+final case class for_luminor(CustomerID_countryLevel:Int,
+                       CustomerGlobalID_panBalticLevel:Int,
+                       CustomerID_AsPerSystem:String,
+                       CustomerGender:String,
+                       IsCustomerALegalPerson:String,
+                       BirthDate:String,
+                       IsCustomerActive_hasValidAgreement:String,
+                       IsResident:String,
+                       ResidenceCountryCode:String,
+                       NACECode:String,
+                       NumberOfLoans:Int,
+                       NumberOfLeasingContracts:Int,
+                       NumberOfDeposits:Int,
+                       SourceSystemID:String,
+                       CountryFromWhichIsCustomer:String)
